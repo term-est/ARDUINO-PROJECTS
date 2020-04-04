d93dd94e16dcb2858da84bd7fe25d58db36f39cb
term-est <eurasia.sely@gmail.com>
2020-04-04 23:52:08 +0300

    minor fixes

--- ardunio_deneme_1.ino
+++ ardunio_deneme_1.ino
@@ -1,13 +1,15 @@
 
-void setup() {
- pinMode(8,OUTPUT);
+void setup()
+{
+	pinMode(8, OUTPUT);
 }
 
-void loop() {
-    digitalWrite(8,HIGH);
-    delay(500);
-    digitalWrite(8,LOW);
-    delay(500);
-    }
+void loop()
+{
+	digitalWrite(8, HIGH);
+	delay(500);
+	digitalWrite(8, LOW);
+	delay(500);
+}
   
   
--- ardunio_deneme_2.ino
+++ ardunio_deneme_2.ino
@@ -1,19 +1,15 @@
-#define Buton 8
-#define Led 10
+#define BUTTON	8
+#define LED		10
 
-int buton_durumu =0;
-
-void setup() {
- pinMode(Buton,INPUT);
- pinMode(Led,OUTPUT);
+void setup()
+{
+	pinMode(BUTTON, INPUT);
+	pinMode(LED, OUTPUT);
 }
 
-void loop() {
-  buton_durumu = digitalRead(Buton);
-  if(buton_durumu==1){
-    digitalWrite(Led,HIGH);
-    }
-  else{
-    digitalWrite(Led,LOW);
-    }
+void loop()
+{
+	digitalWrite(LED, digitalRead(BUTTON));
 }
+
+
