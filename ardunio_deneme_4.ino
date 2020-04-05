dc06080924b3f8b16662e6eceeab82d78db5f61b
term-est <eurasia.sely@gmail.com>
2020-04-05 11:49:46 +0300

    bug fixes

--- ardunio_deneme_4.ino
+++ ardunio_deneme_4.ino
@@ -1,16 +1,15 @@
-#define POTANTIOMETER A0
+#define POTANTIOMETER 	A0
+#define RESOLUTION_COEF 5.00 / 1024.00
 
 
 void setup() 
 {
 	Serial.begin(9600);
-	analogReadResolution(10);
-	analogReference(DEFAULT);	
 	Serial.println("Potansiyometre deger okuma"); 
 }
 
 void loop()
 {
-	Serial.println(5.00 / 1024.00 * analogRead(POTANTIOMETER));
+	Serial.println(RESOLUTION_COEF * analogRead(POTANTIOMETER));
 	delay(300);
 }
